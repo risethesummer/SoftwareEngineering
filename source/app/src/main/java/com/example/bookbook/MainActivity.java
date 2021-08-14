d87e6f53b75502ad7e5b108bad0ac62a3007c135
xmax2807 <63687756+xmax2807@users.noreply.github.com>
2021-08-14 22:55:01 +0700

    update

--- source/app/src/main/java/com/example/bookbook/MainActivity.java
+++ source/app/src/main/java/com/example/bookbook/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -32,14 +34,27 @@ public class MainActivity extends AppCompatActivity {
                 System.exit(0);
             }
         });
+
+        findViewById(R.id.MyAccountButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AccountSetting.class));
+            }
+        });
     }
 
     private void LogOut() {
-        String url = "https://bookbook3wishes.azurewebsites.net/api/account/logout/" + SignIn.user.getId();
+        String url = "https://bookbook3wishes.azurewebsites.net/api/account/logout/" + SignIn.user.getSessionID();
         StringRequest req = new StringRequest(Request.Method.GET, url, null, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                NetworkResponse response = error.networkResponse;
+                if (response.statusCode == 400){
+                    Toast.makeText(MainActivity.this, "This account is already logged in", Toast.LENGTH_LONG).show();
+                }
+                else if (response.statusCode == 403){
+                    Toast.makeText(MainActivity.this, "The server is closed", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
